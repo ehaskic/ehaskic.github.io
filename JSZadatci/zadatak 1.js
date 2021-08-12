662eb3918862e0767332305ceb05d8fd3c82e28c
ehaskic <edinhaskic90@gmail.com>
2021-08-12 23:56:10 +0200

    Dodan folder JSZadatci

---
+++ JSZadatci/zadatak 1.js
@@ -0,0 +1,5 @@
+/* Deklarirati 4 varijable, dodijeliti im proizvoljne numeričke vrijednosti. Pomoću console.log funkcije ispišite zbroj prve i treće varijable, ispišite zatim rezultat dijeljenja druge i četvrte varijable. */
+
+var a = 20, b = 15, c = 10, d = 5;
+console.log("Zbroj prve i treće varijable iznosi:" + (a + c));
+console.log("Rezultat dijeljenja druge i četvrte varijable iznosi:" + (b / d));
---
+++ JSZadatci/zadatak 2.js
@@ -0,0 +1,9 @@
+/* Ako su deklarirane varijable var a = 4, b = 44, c = 24, d = "44". Napišite koji je rezultat operacija ( true ili false ): */
+
+a) console.log( (a > c) ); - false
+b) console.log( (a > b) || (b > c) ); - true
+c) console.log( (a < b) && (a < c) ); - true
+d) console.log( (c > b) && (b < a) ); - false
+e) console.log( !(a > b) || ( (a > c) && (c > b) ) ); - true
+f) console.log( !(a > b) ); - true
+g) console.log( (b === d) ); - false
