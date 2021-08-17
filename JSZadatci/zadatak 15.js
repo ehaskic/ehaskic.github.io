f6018cab53850901c9f27a23df84820db41edafc
ehaskic <edinhaskic90@gmail.com>
2021-08-17 22:45:13 +0200

    Dodan zadatak 15

---
+++ JSZadatci/zadatak 15.js
@@ -0,0 +1,20 @@
+/* Zatražiti od korisnika da unose broj manji od 20. Zatim ispisati sve brojeve od unesenog broja do 1. Ako je unio veći broj, ispisati uneseni broj. */
+
+var a = parseInt(prompt("Unesite broj manji od 20"));
+var b = 0;
+
+if (a<20 && a>0) {
+
+for (var a; a>b; a--) {
+
+console.log(a);
+
+
+}
+}
+
+else {
+
+console.log(a);
+
+}
