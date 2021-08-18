add900ca79131a67356a1c12d293888ef70ec3c2
ehaskic <edinhaskic90@gmail.com>
2021-08-18 22:12:38 +0200

    Dodan je zadatak 17

---
+++ JSZadatci/zadatak 17.js
@@ -0,0 +1,14 @@
+/*Učitati dva broja, zatim ispisati svaki drugi broj između njih koji je djeljiv sa 2 i 3.*/
+
+var a=1;
+var b=100;
+
+for (a=1; a<b; a++) {
+
+if (a%3==0 && a%2==0) {
+
+console.log(a+",");
+
+}
+
+}
