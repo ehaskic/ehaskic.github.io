af2195ec1dd7ef357f8fcac960b313f97f9d9294
ehaskic <edinhaskic90@gmail.com>
2021-08-22 23:23:46 +0200

    Dodan je zadatak 23

---
+++ JSZadatci/zadatak 23.js
@@ -0,0 +1,13 @@
+var a = 3, b = 18, c = “6”;
+for( var i = a; i < b; i += 3 ){
+5
+if( i = = c ){
+console.log( i + " = " + c );
+}
+if( b % i != = 0 ){
+console.log( i - 1 );
+}
+else{
+console.log( i );
+}
+}
